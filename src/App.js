fae893b70e7f80b9e3c916aa479a8bdaf2a1c202
Daaki Benjamin Richard <richarddaaki4@gmail.com>
2022-09-25 02:24:33 +0300

    used Map Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
 
 class App extends Component {
+
   render() {
+    function hello(){
+      alert('Hello');
+    }
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={hello}>Hello</button>
+        <Map />
       </div>
     );
   }
